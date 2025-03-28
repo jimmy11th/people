473b8ad5318f99f55ee7dd6d419fe1c51b9b9783
jimmy.chen <jimmy.chen@nirvana-info.com>
2025-03-28 16:43:18 +0800

    temp

--- src/root.component.js
+++ src/root.component.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import PeoplePage from "./people-page/people-page.component.js";
-
-const router = createBrowserRouter([
-  {
-    path: "/people/:personId",
-    element: <PeoplePage />,
-  },
-  {
-    path: "/people",
-    element: <PeoplePage />,
-    exact: true,
-  },
-]);
 
 export default function Root(props) {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      Warrior Trading
+      <button
+        onClick={() => {
+          alert("clicked Warrior Trading");
+        }}
+      >
+        Click me
+      </button>
+    </>
+  );
 }
